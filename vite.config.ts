b936a14a2491100066cf2bea52a066cb2235a845
Tomas Lubertino <tomas.lubertino@gmail.com>
2023-08-10 17:12:22 -0300

    Changed workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/emme-studio/',
   plugins: [react()],
+  base: '/emme-studio/',
 });
